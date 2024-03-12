43deb9151bb69fed254cb2a4ef4146f9c3aa5f94
lsupwp <lsupwp@gmail.com>
2024-03-12 15:18:57 +0700

    finish

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React, {useState, useEffect} from "react";
+
+export default function App(){
+    const [users, setUsers ] = useState([])
+    useEffect(() =>{
+        fetch(import.meta.env.VITE_API
+            +'/users')
+            .then(res => res.json())
+            .then(result =>{
+                setUsers(result)
+                console.log(result)
+            })
+    },[])
+    return(
+        <div>
+            <ul>
+                {users.map(user => (
+                    <li>{user.id} {user.name} {user.email}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
